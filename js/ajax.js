21dc65770b84a17e79621fdc04537fdf6abc9d19
kunjiyu <138292519+jiyu3984@users.noreply.github.com>
2025-01-11 10:06:13 +0800

    添加翻译

--- js/ajax.js
+++ js/ajax.js
@@ -120,6 +120,18 @@ function ajaxUrl(music, callback)
                 console.debug("歌曲链接：" + jsonData.url);
             }
             
+            // 解决网易云音乐部分歌曲无法播放问题
+            if(music.source == "netease") {
+                if(jsonData.url === "") {
+                    jsonData.url = "https://music.163.com/song/media/outer/url?id=" + music.id + ".mp3";
+                } else {
+                    jsonData.url = jsonData.url.replace(/m7c.music./g, "m7.music.");
+                    jsonData.url = jsonData.url.replace(/m8c.music./g, "m8.music.");
+                }
+            } else if(music.source == "baidu") {    // 解决百度音乐防盗链
+                jsonData.url = jsonData.url.replace(/http:\/\/zhangmenshiting.qianqian.com/g, "https://gss0.bdstatic.com/y0s1hSulBw92lNKgpU_Z2jR7b2w6buu");
+            }
+            
             if(jsonData.url === "") {
                 music.url = "err";
             } else {
@@ -286,32 +298,41 @@ function ajaxPlayList(lid, id, callback) {
 // 参数：音乐ID，回调函数
 function ajaxLyric(music, callback) {
     lyricTip('歌词加载中...');
-    
-    if(!music.lyric_id) callback('');  // 没有歌词ID，直接返回
-    
+
+    if (!music.lyric_id) callback(''); // 没有歌词ID，直接返回
+
     $.ajax({
         type: mkPlayer.method,
         url: mkPlayer.api,
         data: "types=lyric&id=" + music.lyric_id + "&source=" + music.source,
-        dataType : "jsonp",
-        success: function(jsonData){
+        dataType: "jsonp",
+        success: function(jsonData) {
             // 调试信息输出
             if (mkPlayer.debug) {
                 console.debug("歌词获取成功");
             }
-            
             if (jsonData.lyric) {
-                callback(jsonData.lyric, music.lyric_id);    // 回调函数
+                rem.orgLyric = jsonData.lyric;
+                if (jsonData.tlyric) {
+                    rem.orgtLyric = jsonData.tlyric;
+                    //修改处：将tlyric传递给callback
+                    callback(jsonData.lyric, music.lyric_id, jsonData.tlyric); // 回调函数
+                } else {
+                    rem.orgtLyric = null;
+                    callback(jsonData.lyric, music.lyric_id); // 回调函数
+                }
             } else {
-                callback('', music.lyric_id);    // 回调函数
+                rem.orgLyric = null;
+                rem.orgtLyric = null;
+                callback('', music.lyric_id); // 回调函数
             }
-        },   //success
+        }, //success
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            layer.msg('歌词读取失败 - ' + XMLHttpRequest.status);
-            console.error(XMLHttpRequest + textStatus + errorThrown);
-            callback('', music.lyric_id);    // 回调函数
-        }   // error   
-    });//ajax
+                layer.msg('歌词读取失败 - ' + XMLHttpRequest.status);
+                console.error(XMLHttpRequest + textStatus + errorThrown);
+                callback('', music.lyric_id); // 回调函数
+            } // error   
+    }); //ajax
 }
 
 
